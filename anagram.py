7700784075f43ba27e3b50d8ffa30d817927eb82
swetha253 <130964034+swetha253@users.noreply.github.com>
2023-04-17 22:01:57 +0530

    Create anagram.py
    this is a small task

---
+++ anagram.py
@@ -0,0 +1,8 @@
+def anagram(s1,s2):
+  if(sorted(s1)==sorted(s2)):
+    print("The strings are anagrams.")
+  else:
+    print("The strings aren't anagrams.")
+s1="listen"
+s2="silent"
+anagram(s1,s2)
